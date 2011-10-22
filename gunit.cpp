07d471ff27e2a412c170cc662ac6e814b55f7e38
Tianyu Zhu <tian.tian098@gmail.com>
2011-10-22 11:11:01 -0400

    Initial commit.

---
+++ gunit.cpp
@@ -0,0 +1,119 @@
+#include "q1upp.h"
+#include "q1gunit.h"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cstdarg>
+#include <cstdio>
+#include <exception>
+
+using namespace std;
+
+struct AssertException {
+    char const*msg;
+
+    AssertException(char const*msg) :
+        msg(msg? strdup(msg):NULL) {}
+
+    ~AssertException() { delete msg; }
+};
+
+vector<Test*>tests = vector<Test*>();
+
+Test::Test(char const*_name): name(_name) {
+    tests.push_back(this);
+}
+
+void Test::setup() {}
+
+void Test::cleanup() throw() {}
+
+void uMain::main() {
+    int fails = 0, errors = 0;
+    for (vector<Test*>::iterator i=tests.begin(); i<tests.end(); ++i) {
+        Test &test = **i;
+        try {
+            cout << "Testing " << setw(65) << setfill('.') << left << test.name;
+            cout.flush();
+            test.setup();
+            test.execute();
+            cout << ".Passed" << endl;
+        } catch (AssertException &e) {
+            ++fails;
+            cout << ".FAILED" << endl;
+            if (e.msg) cout << e.msg << endl;
+            test.cleanup();
+        } catch (exception &e) {
+            ++errors;
+            cout << ".FAILED\nEncountered EXCEPTION" << endl;
+            if (e.what()) cout << e.what() << endl;
+            test.cleanup();
+        } catch (...) {
+            ++errors;
+            cout << "..ERROR" << endl;
+            test.cleanup();
+            throw;
+        }
+    }
+
+    if (fails) cerr << fails << " of " << tests.size() << " tests FAILED." << endl;
+    if (errors) cerr << errors << " of " << tests.size() << " tests ended with ERROR." << endl;
+    uRetCode = fails + errors;
+}
+
+namespace Assert {
+
+void Fail(char const*msg) {
+    throw AssertException(msg);
+}
+
+void Fail(char const*msg, char const*format, ...) {
+    static char buf[4096];
+    char *b = buf;
+
+    // If there is a message, copy it to the buffer.
+    if (msg) {
+        while (*msg) *(b++) = *(msg++);
+        *(b++) = ':';
+        *(b++) = '\n';
+    }
+
+    va_list msgs;
+    va_start(msgs, format);
+    vsprintf(b, format, msgs);
+    va_end(msgs);
+    Fail(buf);
+}
+
+void True(bool expr, const char *msg) {
+    if (expr) return;
+    Fail(msg, "%s", "Expected true but was false");
+}
+
+void False(bool expr, const char *msg) {
+    if (!expr) return;
+    Fail(msg, "%s", "Expected false but was true");
+}
+
+template<>
+void Equal<int, int>
+(int expected, int tested, char const*msg) {
+    if (expected == tested) return;
+    Fail(msg, "Expected: %d, Got: %d", expected, tested);
+}
+
+template<>
+void Equal<char const*, char const*>
+(char const*expected, char const*tested, char const*msg) {
+    if (expected == tested) return;
+    if (expected == NULL || tested == NULL) goto fail;
+    for (const char *e = expected, *t = tested; *e && *t;)
+        if (*e++ != *t++) goto fail;
+    return;
+
+fail:
+    Fail(msg, "Expected: \"%s\"\nGot: \"%s\"", expected, tested);
+}
+
+} // namespace Assert
+
---
+++ gunit.h
@@ -0,0 +1,59 @@
+/*
+G-Unit is a small unit testing library.
+
+Copyright (C) 2011 Tianyu Zhu
+
+Define tests by defining child-classes of the Test structure. Each Test object
+that is created will be run as a test. Functions defined in the Assert
+namespace can be used while executing the test to safetly assert for
+conditions. For example:
+
+// Definition of MyTest
+struct MyTest : public Test {
+    MyTest(char const*file) : file(file), Test("My Test") {}
+    void execute() const {
+        // Open the file ...
+        Assert::True(filestream.open(file), "Open the file stream");
+        // Run the test with contents of file ...
+        Assert:: ...
+    }
+private:
+    char const*file;
+};
+
+// Run MyTest using file1.txt and file2.txt as input.
+MyTest test_with_file1("file1.txt"), test_with_file2("file2.txt");
+
+Note that G-Unit already defines the main function in its implementation.
+
+*/
+
+#ifndef GUNIT_H
+#define GUNIT_H
+
+#include <cstring>
+
+struct Test {
+    Test(char const*_name);
+    char const*const name;
+    virtual void setup();
+    virtual void execute() = 0;
+    virtual void cleanup() throw();
+};
+
+namespace Assert {
+
+void Fail(char const*msg, char const*format, ...);
+
+void Fail(char const*msg = NULL);
+
+void True(bool expr, char const*msg = NULL);
+
+void False(bool expr, char const*msg = NULL);
+
+template<typename T1, typename T2>
+void Equal(T1 expected, T2 tested, char const*msg = NULL);
+
+} // namespace Assert
+
+#endif // GUNIT_H
